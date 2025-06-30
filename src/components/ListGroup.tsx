90dfe93f6b6b54cd766347f174e36156b428c3a1
= <=>
2025-06-30 16:06:05 +0800

    added state to listgroup onClick

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,17 +1,16 @@
-import { MouseEvent } from "react";
+import { useState } from "react";
 
 function ListGroup() {
   let items = ["Chicken", "Pork", "Beef", "Mutton", "Fish"];
+  // State Hook (Tells React DOM that this is a dyanic variable index 0 is variable index 1 is method to change it)
+  const [selectedIndex, setSelectedIndex] = useState(-1);
   
 
   //const Message = items.length === 0 ? <p>No item found</p> : null
-
   const getMessage = () => {
     //in JS using && if 1st condition is true will render the 2nd condition
     return items.length === 0 && <p>No item found</p>
   }
-  //Event handler, ( :MouseEvent) is a type annotation in TS for Reacts own onClick mouse event
-  const handleClick = (event : MouseEvent) => console.log(event)
 
   //<> tells React that its a fragment. (needed when there is multiple elements like h1 and ul in this case)
   //with {} you can add variables or functions into jsx markup
@@ -21,8 +20,8 @@ function ListGroup() {
       <h1>List</h1>
       {getMessage()}
       <ul className="list-group">
-        {items.map((item) => (
-          <li className="list-group-item"key={item} onClick={handleClick}>{item}</li>
+        {items.map((item,index) => (
+          <li className= {selectedIndex === index ? 'list-group-item active' : 'list-group-item'} key={item} onClick={() => setSelectedIndex(index)}>{item}</li>
         ))}
       </ul>
     </>
