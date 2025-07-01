639e3f26bc1d6f97c2d9e1f368d528e7af97cdd7
= <=>
2025-07-01 11:41:43 +0800

    Added functionality between alert, list and button. Added dismissible button for alert.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
+import { useState } from "react";
 import Alert from "./components/Alert";
+import Button from "./components/Button";
 import ListGroup from "./components/ListGroup";
 
 function App() {
   let items = ["Chicken", "Pork", "Beef", "Mutton", "Fish"];
+  const [selectedItem, setSelectedItem] = useState("No item selected");
 
   const handleSelectItem = (item: string) => {
-    console.log(item);
+    setSelectedItem(item)
   };
+
+  const [alertVisible,setAlertVisibility] = useState(false);
+
 
   return (
     <div>
       <ListGroup items={items} heading="Meat" onSelectItem={handleSelectItem} />
-      <Alert>
-         <h1>Hello World</h1>
-      </Alert>
+      {alertVisible && <Alert onClose={() => setAlertVisibility(false)}> {selectedItem} </Alert>}
+      <Button color="dark" onClick={() => setAlertVisibility(true)}>
+        My Button
+      </Button>
     </div>
   );
 }
--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,14 +1,17 @@
-import type { ReactNode } from "react"
+import type { ReactNode } from "react";
 
 interface Props {
-    children: ReactNode;
+  children: ReactNode;
+  onClose: () => void;
 }
 
-const Alert = ({children}:Props) => {
+const Alert = ({ children, onClose }: Props) => {
   return (
-    <div className="alert alert-primary">{children}</div>
-    
-  )
-}
+    <div className="alert alert-primary alert-dismissible">
+      {children}
+      <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close" onClick={onClose}></button>
+      </div>
+  );
+};
 
-export default Alert
+export default Alert;
