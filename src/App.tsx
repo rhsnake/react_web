bf9c7807f47acc37b80942a0bfadd14bddcb5655
= <=>
2025-07-02 12:52:11 +0800

    added layout.tsx and navbar to it to make it persist between pages. Removed Navbar from home

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import './App.css';
+import "./App.css";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Pages/home";
-import Help from './Pages/help';
+import Help from "./Pages/help";
+import Layout from "./Layout";
 
 function App() {
-
   return (
     <Router>
       <Routes>
-        <Route path="/" element = {<Home />}/>
-        <Route path="/help" element = {<Help />}/>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/help" element={<Help />} />
+        </Route>
       </Routes>
     </Router>
   );
--- src/Pages/home.tsx
+++ src/Pages/home.tsx
@@ -3,32 +3,35 @@ import Alert from "../components/Alert";
 import Button from "../components/Button";
 import ListGroup from "../components/ListGroup";
 import { Link } from "react-router-dom";
-import NavBar from "../components/NavBar";
 
 function Home() {
   let items = ["Chicken", "Pork", "Beef", "Mutton", "Fish"];
   const [selectedItem, setSelectedItem] = useState("No item selected");
 
   const handleSelectItem = (item: string) => {
-    setSelectedItem(item)
+    setSelectedItem(item);
   };
 
-  const [alertVisible,setAlertVisibility] = useState(false);
-
+  const [alertVisible, setAlertVisibility] = useState(false);
 
   return (
     <>
-    <NavBar>
-
-    </NavBar>
-    <div>
-      <ListGroup items={items} heading="Meat" onSelectItem={handleSelectItem} />
-      {alertVisible && <Alert onClose={() => setAlertVisibility(false)}> {selectedItem} </Alert>}
-      <Button color="dark" onClick={() => setAlertVisibility(true)}>
-        My Button
-      </Button>
-      <Link to= '/help'>Help Page</Link>
-    </div>
+      <div>
+        <ListGroup
+          items={items}
+          heading="Meat"
+          onSelectItem={handleSelectItem}
+        />
+        {alertVisible && (
+          <Alert onClose={() => setAlertVisibility(false)}>
+            {selectedItem}
+          </Alert>
+        )}
+        <Button color="dark" onClick={() => setAlertVisibility(true)}>
+          My Button
+        </Button>
+        <Link to="/help">Help Page</Link>
+      </div>
     </>
   );
 }
